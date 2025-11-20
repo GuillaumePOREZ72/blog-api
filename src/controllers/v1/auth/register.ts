4e00c64781ab19b78e5062ba4d3cc799be6a2238
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-11-20 09:46:49 +0100

    commit de base

---
+++ src/controllers/v1/auth/register.ts
@@ -0,0 +1,54 @@
+/**
+ * Custom modules
+ */
+import { logger } from '@/lib/winston';
+import config from '@/config';
+import { genUsername } from '@/utils';
+
+/**
+ * Models
+ */
+import User from '@/models/user';
+
+/**
+ * Types
+ */
+import type { Request, Response } from 'express';
+import { IUser } from '@/models/user';
+
+type UserData = Pick<IUser, 'email' | 'password' | 'role'>;
+
+const register = async (req: Request, res: Response): Promise<void> => {
+  const { email, password, role } = req.body as UserData;
+
+  try {
+    const username = genUsername();
+
+    const newUser = await User.create({
+      username,
+      email,
+      password,
+      role,
+    });
+
+    // Generate access token and refresh token for new user
+
+    res.status(201).json({
+      user: {
+        username: newUser.username,
+        email: newUser.email,
+        role: newUser.role,
+      },
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: error,
+    });
+
+    logger.error('Error during user registration', error);
+  }
+};
+
+export default register;
---
+++ src/routes/v1/index.ts
@@ -0,0 +1,19 @@
+import { timeStamp } from 'console';
+import { Router } from 'express';
+
+const router = Router();
+
+import authRoutes from '@/routes/v1/auth';
+
+router.get('/', (req, res) => {
+  res.status(200).json({
+    message: 'API is live',
+    status: 'ok',
+    version: '1.0.0',
+    timestamp: new Date().toISOString(),
+  });
+});
+
+router.use('/auth', authRoutes);
+
+export default router;
