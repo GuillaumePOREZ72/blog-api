631e9435cd0e2fb876727b41f01c416a69d903b6
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-11-24 12:22:38 +0100

    ✨ feat: add blog retrieval endpoints; implement getAllBlogs, getBlogsByUser, and getBlogBySlug controllers

---
+++ src/controllers/v1/blog/get_blog_by_slug.ts
@@ -0,0 +1,65 @@
+/**
+ * Custom modules
+ */
+import { logger } from '@/lib/winston';
+
+/**
+ * Models
+ */
+import Blog from '@/models/blog';
+import user from '@/models/user';
+import User from '@/models/user';
+
+/**
+ * Types
+ */
+import type { Request, Response } from 'express';
+
+const getBlogBySlug = async (req: Request, res: Response): Promise<void> => {
+  try {
+    const userId = req.params.userId;
+    const slug = req.params.slug;
+
+    const user = await User.findById(userId).select('role').lean().exec();
+
+    const blog = await Blog.findOne({ slug })
+      .select('-banner.publicId -__v')
+      .populate('author', '-createdAt -updatedAt -__v')
+      .lean()
+      .exec();
+
+    if (!blog) {
+      res.status(404).json({
+        code: 'NotFound',
+        message: 'Blog not found',
+      });
+      return;
+    }
+
+    if (user?.role === 'user' && blog.status === 'draft') {
+      res.status(403).json({
+        code: 'AuthorizationError',
+        message: 'Access denied, insufficient permissions',
+      });
+
+      logger.warn('A user tried to access a draft blog', {
+        userId,
+        blog,
+      });
+    }
+
+    res.status(200).json({
+      blog,
+    });
+  } catch (error) {
+    res.status(500).json({
+      code: 'ServerError',
+      message: 'Internal server error',
+      error: error,
+    });
+
+    logger.error('Error while fetching blog by slug', error);
+  }
+};
+
+export default getBlogBySlug;
--- src/routes/v1/blog.ts
+++ src/routes/v1/blog.ts
@@ -14,6 +14,9 @@ import uploadBlogBanner from '@/middlewares/uploadBlogBanner';
  * Controllers
  */
 import createBlog from '@/controllers/v1/blog/create_blog';
+import getAllBlogs from '@/controllers/v1/blog/get_all_blogs';
+import getBlogsByUser from '@/controllers/v1/blog/get_blogs_by_user';
+import getBlogBySlug from '@/controllers/v1/blog/get_blog_by_slug';
 
 const upload = multer();
 
@@ -24,7 +27,6 @@ router.post(
   authenticate,
   authorize(['admin']),
   upload.single('banner_image'),
-  uploadBlogBanner('post'),
   body('title')
     .trim()
     .notEmpty()
@@ -37,7 +39,50 @@ router.post(
     .isIn(['draft', 'published'])
     .withMessage('Status must be one of the value, draft or published.'),
   validationError,
+  uploadBlogBanner('post'),
   createBlog,
+);
+
+router.get(
+  '/',
+  authenticate,
+  authorize(['admin', 'user']),
+  query('limit')
+    .optional()
+    .isInt({ min: 1, max: 50 })
+    .withMessage('Limit must be between 1 to 50'),
+  query('offset')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Offset must be a positive integer'),
+  validationError,
+  getAllBlogs,
+);
+
+router.get(
+  '/user/:userId',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('userId').isMongoId().withMessage('Invalid user ID'),
+  query('limit')
+    .optional()
+    .isInt({ min: 1, max: 50 })
+    .withMessage('Limit must be between 1 to 50'),
+  query('offset')
+    .optional()
+    .isInt({ min: 0 })
+    .withMessage('Offset must be a positive integer'),
+  validationError,
+  getBlogsByUser,
+);
+
+router.get(
+  '/:slug',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('slug').notEmpty().withMessage('Slug is required'),
+  validationError,
+  getBlogBySlug,
 );
 
 export default router;
