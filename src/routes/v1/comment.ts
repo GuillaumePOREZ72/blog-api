c159c646a88bc961a5af2553b7b10b9e57d14a9c
GuillaumePOREZ72 <guillaume.porez@oclock.school>
2025-11-25 14:50:14 +0100

    ✨ feat: implement getCommentsByBlog controller; add functionality to retrieve comments for a specific blog

--- src/routes/v1/comment.ts
+++ src/routes/v1/comment.ts
@@ -10,11 +10,12 @@ import { body, param } from 'express-validator';
 import authenticate from '@/middlewares/authenticate';
 import authorize from '@/middlewares/authorize';
 import validationError from '@/middlewares/validationError';
-import commentBlog from '@/controllers/v1/comment/comment_blog';
 
 /**
  * Controllers
- */
+*/
+import commentBlog from '@/controllers/v1/comment/comment_blog';
+import getCommentsByBlog from '@/controllers/v1/comment/get_comments_by_blog';
 
 const router = Router();
 
@@ -26,6 +27,15 @@ router.post(
   body('content').trim().notEmpty().withMessage('Content is required'),
   validationError,
   commentBlog,
+);
+
+router.get(
+  '/blog/:blogId',
+  authenticate,
+  authorize(['admin', 'user']),
+  param('blogId').isMongoId().withMessage('Invalid blog ID'),
+  validationError,
+  getCommentsByBlog,
 );
 
 export default router;
